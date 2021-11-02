fdb9e6cc7cbff3b958b280316f2d6177c2e13ad7
o-O-MidNight-O-o <86651612+o-O-MidNight-O-o@users.noreply.github.com>
2021-11-03 00:50:53 +0330

    Add files via upload

---
+++ server.py
@@ -0,0 +1,47 @@
+import socket
+import threading
+
+
+HEADER = 64
+PORT = 5050
+# SERVER = "192.168.1.114"
+SERVER = socket.gethostbyname(socket.gethostname())
+# print(SERVER)
+ADDR = (SERVER,PORT)
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+
+
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind(ADDR)
+
+def handleClient(conn , addr):
+    print(f"[NEW CONNECTION] {addr} connected")
+    
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT) #blocking line of code
+        if msg_length:
+            msg_length = int(msg_length)
+            msg =conn.recv(msg_length).decode(FORMAT)
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+            
+            print(f"[{addr}] {msg}")
+            conn.send("msg reseved".encode(FORMAT))
+    conn.close()
+
+def start():
+    server.listen()
+    print("[LISTENING] server is listening on {SERVER}")
+
+    while True:
+        conn , addr = server.accept()
+        thread = threading.Thread(target=handleClient,args=(conn,addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.activeCount()-1}") # - start cuz always running
+
+
+print("[STARTING] server is starting... ")
+start()
