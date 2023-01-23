32c5c263c55a8d67e0dea1032810706c7d670861
o-O-MidNight-O-o <86651612+o-O-MidNight-O-o@users.noreply.github.com>
2023-01-23 23:11:52 +0330

    Update server.py

--- server.py
+++ server.py
@@ -4,14 +4,11 @@ import threading
 
 HEADER = 64
 PORT = 5050
-# SERVER = "192.168.1.114"
+# SERVER = "IP_ADDRESS"
 SERVER = socket.gethostbyname(socket.gethostname())
-# print(SERVER)
 ADDR = (SERVER,PORT)
 FORMAT = "utf-8"
 DISCONNECT_MESSAGE = "!DISCONNECT"
-
-
 
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind(ADDR)
